df10ff984c88a630f4f992e3c24d2ea08156f466
KyleKulas <kylekulas@gmail.com>
2022-02-05 16:57:43 -0800

    Moved repo

--- Week_1/Day_1/test.py
+++ Week_1/Day_1/test.py
@@ -1,7 +1,7 @@
 from collections import ChainMap
 
 dict1 = { 'a' : 1, 'b' : 2 }
-dict2 = { 'c' : 3, 'b' : 4 }
+dict2 = { 'c' : 3, 'b' : 5 }
 chain_map = ChainMap(dict1, dict2)
 print(list(chain_map.keys()))
 print(list(chain_map.values()))
